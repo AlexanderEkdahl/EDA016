8b89cf182657b60d85bd3f4a87fab3c99e1e0118
Alexander Ekdahl <alexander@noviumdesign.se>
2012-10-11 07:40:18 +0200

    Finished lab9

--- lab8-9/src/ContrastFilter.java
+++ lab8-9/src/ContrastFilter.java
@@ -3,63 +3,77 @@ import se.lth.cs.ptdc.images.ImageFilter;
 
 /** IdentityFilter beskriver en identitetstransformation */
 public class ContrastFilter extends ImageFilter {
+
+	Color[] grayLevels;
+
 	/** skapa ett filterobjekt med namnet name */
 	public ContrastFilter(String name) {
 		super(name);
+
+		grayLevels = new Color[256];
+		for(int i = 0; i < 256; i++)
+		{
+			grayLevels[i] = new Color(i,i,i);
+		}
 	}
-	
+
 	/** filtrera bilden i matrisen inPixels och returnera resultatet i
 		en ny matris. Utnyttja eventuellt värdet av parameter */
 	public Color[][] apply(Color[][] inPixels, double paramValue) {
 		int height = inPixels.length;
 		int width = inPixels[0].length;
 		Color[][] outPixels = new Color[height][width];
-		
-		short[][] intensity = computeIntensity(inPixels);
+
+		short[][] intensity = new short[height][width];
 		int[] histogram = new int[256];
-		
-		for(int i = 0; i < height; i++) {
-			for(int j = 0; j < width; j++) {
-				histogram[(int)intensity[i][j]]++;
+		intensity = computeIntensity(inPixels);
+
+		for (int i = 0; i < height; ++i) {
+			for (int j = 0; j < width; ++j) {
+				histogram[intensity[i][j]]++;
 			}
 		}
-		
-		int pixels = (int)Math.round(paramValue / 100 * width * height);
-		
+
 		int lowCut = 0;
-		int sum = histogram[0];
-		
-		while(sum < pixels) {
-			lowCut++;
-			sum += histogram[lowCut];
-		}
-		
-		int highCut = 255;
-		sum = histogram[255];
-		
-		while(sum < pixels) {
-			highCut--;
-			sum += histogram[highCut];
+		int highCut = 0;
+		boolean doneLow = false;
+		boolean doneHigh = false;
+
+		double goal = paramValue/100 * height * width;
+		for (int i = 0, sumLow = 0, sumHigh = 0; i < 256; i++) {
+			sumLow += histogram[i];
+			sumHigh += histogram[255-i];
+			if((goal <= sumLow) && !doneLow)
+			{
+				lowCut = i;
+				doneLow = true;
+			}
+			if((goal <= sumHigh) && !doneHigh)
+			{
+				highCut = 255-i;
+				doneHigh = true;
+			}
+
+			if(doneLow && doneHigh)
+				break;
 		}
 		
 		for (int i = 0; i < height; ++i) {
 			for (int j = 0; j < width; ++j) {
-				int newIntensity = 0;
-				if(intensity[i][j] > highCut) {
-					newIntensity = 255;
-				}
+				if(intensity[i][j] < lowCut)
+					outPixels[i][j] = grayLevels[0];
+				else if(intensity[i][j] > highCut)
+					outPixels[i][j] = grayLevels[255];
 				else {
-					newIntensity = (int)Math.round(255 * (double)((intensity[i][j] - lowCut) / (highCut - lowCut)));
-				}
-				
-				try {
-					outPixels[i][j] = new Color(newIntensity, newIntensity, newIntensity);
-				}
-				catch(Exception e) {
-					System.out.println(newIntensity);
+					try {
+						outPixels[i][j] = grayLevels[255*(intensity[i][j] - lowCut) / (highCut - lowCut)];
+					} catch (java.lang.ArithmeticException e) {
+						outPixels[i][j] = new Color(255,0,9);
+					}
 				}
 			}
 		}
+
 		return outPixels;
 	}
 }
