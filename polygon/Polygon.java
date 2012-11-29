33ead3f456de2c1e46b03f6e5ced60a519dd8f22
Catarina Sörensen <catarinasorensen@Catarinas-MacBook-Air.local>
2012-11-29 18:16:53 +0100

    ..

--- polygon/Polygon.java
+++ polygon/Polygon.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.awt.Color;
 import se.lth.cs.ptdc.window.SimpleWindow;
 
 public class Polygon {
@@ -15,6 +16,8 @@ public class Polygon {
 		w.moveTo((int)Math.round(Math.cos(a)*d)+300, (int)Math.round(Math.sin(a)*d)+300);
 
 		for(int i = 0; i < FEST - 1; i++) {
+			Color c = new Color(255-(int)Math.round((255.0/FEST)*i), 127, (int)Math.round((255.0/FEST)*i));
+			w.setLineColor(c);
 			a += (2*Math.PI)/FEST;
 			d = rand.nextInt(300);
 			w.lineTo((int)Math.round(Math.cos(a)*d)+300, (int)Math.round(Math.sin(a)*d)+300);
