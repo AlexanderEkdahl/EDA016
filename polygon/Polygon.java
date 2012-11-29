33b574e6a3357868e8979a52dbfd4be362ec198d
Catarina Sörensen <catarinasorensen@Catarinas-MacBook-Air.local>
2012-11-29 17:41:06 +0100

    polygon

---
+++ polygon/Polygon.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import se.lth.cs.ptdc.window.SimpleWindow;
+
+public class Polygon {
+	private static final int FEST = 1000;
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		SimpleWindow w = new SimpleWindow(600, 600, "Polygon");
+		double a1 = rand.nextDouble()*2*Math.PI;
+		double a = a1;
+		int d1 = rand.nextInt(300);
+		int d = d1;
+
+		w.moveTo((int)Math.round(Math.cos(a)*d)+300, (int)Math.round(Math.sin(a)*d)+300);
+
+		for(int i = 0; i < FEST - 1; i++) {
+			a += (2*Math.PI)/FEST;
+			d = rand.nextInt(300);
+			w.lineTo((int)Math.round(Math.cos(a)*d)+300, (int)Math.round(Math.sin(a)*d)+300);
+			SimpleWindow.delay(1);
+		}
+
+		w.lineTo((int)Math.round(Math.cos(a1)*d1)+300, (int)Math.round(Math.sin(a1)*d1)+300);
+	}
+}
+
